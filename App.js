0f3251f4c8e21406af269a3771027e0e50d915fb
sw <genie_s2@naver.com>
2018-12-17 15:49:10 +0900

    end

--- App.js
+++ App.js
@@ -9,17 +9,22 @@ import {
 } from "react-native";
 import Weather from "./weather";
 
+const API_KEY = "8d7d216fb52b34f60c9eaba0bbdb4931";
+//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
 export default class App extends Component {
     state = {
         isLoaded: false,
-        error: null
+        error: null,
+        temperature: null,
+        name: null
     };
     componentDidMount() {
         navigator.geolocation.getCurrentPosition(
             position => {
-                this.setState({
-                    isLoaded: true
-                });
+                this._getWeater(
+                    position.coords.latitude,
+                    position.coords.longitude
+                );
             },
             error => {
                 this.setState({
@@ -28,13 +33,29 @@ export default class App extends Component {
             }
         );
     }
+    _getWeater = (lat, lon) => {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+        )
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    temperature: json.main.temp,
+                    name: json.weather[0].main,
+                    isLoaded: true
+                });
+            });
+    };
     render() {
-        const { isLoaded, error } = this.state;
+        const { isLoaded, error, temperature, name } = this.state;
         return (
             <View style={styles.container}>
                 <StatusBar hidden={true} />
                 {isLoaded ? (
-                    <Weather />
+                    <Weather
+                        weatherName={name}
+                        temp={Math.floor(temperature - 273.15)}
+                    />
                 ) : (
                     <View style={styles.loading}>
                         <Text style={styles.loadingText}>
--- weather.js
+++ weather.js
@@ -1,29 +1,90 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class weather extends Component {
-    render() {
-        return (
-            <LinearGradient
-                colors={["#00C6FB", "#005BEA"]}
-                style={styles.container}
-            >
-                <View style={styles.upper}>
-                    <Ionicons color="white" size={144} name="ios-rainy" />
-                    <Text style={styles.temp}>35℃</Text>
-                </View>
-                <View style={styles.lower}>
-                    <Text style={styles.title}>Raining</Text>
-                    <Text style={styles.subtitle}>
-                        For more info look outside
-                    </Text>
-                </View>
-            </LinearGradient>
-        );
+const weatherCases = {
+    Rain: {
+        colors: ["#00C6FB", "#005BEA"],
+        title: "Raining",
+        subtitle: "For more info look outside",
+        icon: "weather-rainy"
+    },
+    Clear: {
+        colors: ["#FEF253", "#FF7300"],
+        title: "Sunny",
+        subtitle: "Go get your ass burnt",
+        icon: "weather-sunny"
+    },
+    Thunderstorm: {
+        colors: ["#00ECBC", "#007ADF"],
+        title: "Thunderstorm",
+        subtitle: "Actually, outside of the house",
+        icon: "weather-lightning"
+    },
+    Clouds: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "I know, fucking boring",
+        icon: "weather-cloudy"
+    },
+    Snow: {
+        colors: ["#7DE2FC", "#B9B6E5"],
+        title: "Snow",
+        subtitle: "Do you want to build a snowman?",
+        icon: "weather-snowy"
+    },
+    Mist: {
+        colors: ["#89F7FE", "#66A8FF"],
+        title: "Mist",
+        subtitle: "Mist...",
+        icon: "weather-fog"
+    },
+    Drizzle: {
+        colors: ["#89F7FE", "#66A8FF"],
+        title: "Drizzle",
+        subtitle: "Drizzle...",
+        icon: "weather-hail"
+    },
+    Haze: {
+        colors: ["#89F7FE", "#66A8FF"],
+        title: "Haze",
+        subtitle: "Haze...",
+        icon: "weather-windy"
     }
+};
+function weather({ weatherName, temp }) {
+    return (
+        <LinearGradient
+            colors={weatherCases[weatherName].colors}
+            style={styles.container}
+        >
+            <View style={styles.upper}>
+                <MaterialCommunityIcons
+                    color="white"
+                    size={150}
+                    name={weatherCases[weatherName].icon}
+                />
+                <Text style={styles.temp}>{temp}℃</Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>
+                    {weatherCases[weatherName].title}
+                </Text>
+                <Text style={styles.subtitle}>
+                    {weatherCases[weatherName].subtitle}
+                </Text>
+            </View>
+        </LinearGradient>
+    );
 }
+
+weather.PropTypes = {
+    temp: PropTypes.number.isRequired,
+    weatherName: PropTypes.string.isRequired
+};
+export default weather;
 
 const styles = StyleSheet.create({
     container: {
